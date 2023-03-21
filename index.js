2c3083ef919a54fbe1a67f9aaf86a124bc294904
Bruce <bruce.hillsworth@gmail.com>
2023-03-21 15:31:24 -0600

    Pass tests on index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+function receivesAFunction(callback) {
+	callback();
+}
+
+
+function returnsANamedFunction() {
+	return function namedFunction() {
+		console.log("I am the named function!");
+	}
+}
+
+function returnsAnAnonymousFunction() {
+	return function() {
+		console.log("I am an anonymous function!");
+	}
+}
